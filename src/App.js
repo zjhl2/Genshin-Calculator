a218c4fe4910f9a98c6acdc08fdd0f0a371bc363
zjhl2 <478791469@qq.com>
2023-11-28 14:30:47 +0800

    增加抗性等级增伤乘区

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from './LoginPage';
 import DashboardPage from './DashboardPage';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/" element={<Navigate to="/dashboard" />} />
-            </Routes>
-        </Router>
+        <DashboardPage />
     );
 }
 
--- src/DashboardPage.js
+++ src/DashboardPage.js
@@ -32,6 +32,7 @@ const buttonGroupStyle = {
     gap: '10px',
     alignItems: 'center',
 };
+const STORAGE_KEY = 'Genshin-Calculator-dashboardTableData-v3';
 function DashboardPage() {
     const [tableData, setTableData] = useState([
         { id: 0, name: "暴击率", content: '70', step: 3.5 },
@@ -42,18 +43,59 @@ function DashboardPage() {
         { id: 5, name: "基础攻击力", content: '1000', step: 0 }, // 这里基础攻击力的 step 设置为 0
         { id: 6, name: "基础生命值", content: '15000', step: 0 }, // 这里基础生命值的 step 设置为 0
         { id: 7, name: "倍率", content: '1', step: 0 }, // 这里倍率的 step 设置为 0
+        { id: 8, name: "角色等级", content: '90', step: 5 }, // 这里角色等级的 step 设置为 0
+        { id: 9, name: "怪物等级", content: '90', step: 5 }, // 这里怪物等级的 step 设置为 0
+        { id: 10, name: "怪物抗性", content: '10', step: 5 }, // 这里怪物抗性的 step 设置为 5
+        { id: 11, name: "增伤", content: '0', step: 5 }, // 这里怪物抗性的 step 设置为 5
     ]);
+    const saveDataToLocalStorage = (data) => {
+        try {
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+        } catch (error) {
+            console.error("Error saving data to localStorage:", error);
+        }
+    };
 
+    const getDataFromLocalStorage = () => {
+        try {
+            const storedData = localStorage.getItem(STORAGE_KEY);
+            return storedData ? JSON.parse(storedData) : null;
+        } catch (error) {
+            console.error("Error retrieving data from localStorage:", error);
+            return null;
+        }
+    };
+
+    useEffect(() => {
+        const storedData = getDataFromLocalStorage();
+        if (storedData && storedData.length === tableData.length) {
+            setTableData(storedData);
+        }
+    }, []);
+
+    useEffect(() => {
+        calculateDmg();
+    }, [tableData]);
     const handleInputChange = (value, id) => {
+        // 移除可能的负号
+        const sanitizedValue = value.replace(/^-/, '');
+
+        // 重新构造正则表达式，用于检查移除负号后的值
         const regex = /^\d+(\.\d{0,2})?$/;
-        if (!regex.test(value) && value !== '') {
+
+        if (!regex.test(sanitizedValue) && sanitizedValue !== '') {
             return;
         }
 
+        // 如果输入是负数，将负号添加回去
+        const updatedValue = value.startsWith('-') ? `-${sanitizedValue}` : sanitizedValue;
+
         const updatedTableData = tableData.map((item) =>
-            item.id === id ? { ...item, content: value } : item
+            item.id === id ? { ...item, content: updatedValue } : item
         );
+
         setTableData(updatedTableData);
+        saveDataToLocalStorage(updatedTableData);
     };
 
     useEffect(() => {
@@ -85,7 +127,18 @@ function DashboardPage() {
         const baseATK = parseFloat(tableData[5].content);
         const baseHP = parseFloat(tableData[6].content);
         const percent = parseFloat(tableData[7].content);
+        const characterLevel = parseFloat(tableData[8].content);
+        const monsterLevel = parseFloat(tableData[9].content);
+        const res = parseFloat(tableData[10].content);
+        const plus = parseFloat(tableData[11].content);
 
+        let resAndLevel = (5*characterLevel+500) / (5*monsterLevel+500 + 5*characterLevel+500) * (1 + plus/100);
+        if (res <= 0)
+            resAndLevel *= 1 - res/100/2;
+        if (res > 0 && res <= 75)
+            resAndLevel *= 1 - res/100;
+        if (res > 75)
+            resAndLevel *= 1/(1+4*res/100);
         const attributeToUse = calculationMode === 'ATK' ? ATK : MaxHP;
 
         ATK = baseATK * (1 + (ATK - 100) / 100);
@@ -100,8 +153,8 @@ function DashboardPage() {
         if (ElementalMastery > 0) {
             emv += 2.78 / (1 + 1400 / ElementalMastery);
         }
-        expectDmg = attributeToUse * percent * critExpectv;
-        critDmg = attributeToUse * percent * (1 + CRITDMG / 100);
+        expectDmg = attributeToUse * percent * critExpectv * resAndLevel;
+        critDmg = attributeToUse * percent * (1 + CRITDMG / 100) * resAndLevel;
         setDirect(0);
         setElement(0);
         if (damageMultiplier === "1.5蒸发融化") {
@@ -113,14 +166,14 @@ function DashboardPage() {
             critDmg *= 2 * emv;
         }
         if (damageMultiplier === "1.15超激化90级") {
-            expectDmg = (attributeToUse * percent + 1447 * 1.15 * emv) * critExpectv;
-            critDmg = (attributeToUse * percent + 1447 * 1.15 * emv) * (1 + CRITDMG / 100);
+            expectDmg = (attributeToUse * percent + 1447 * 1.15 * emv) * critExpectv * resAndLevel;
+            critDmg = (attributeToUse * percent + 1447 * 1.15 * emv) * (1 + CRITDMG / 100) * resAndLevel;
             direct = attributeToUse * percent / ((attributeToUse * percent + 1447 * 1.15 * emv));
             element = 1 - direct;
         }
         if (damageMultiplier === "1.25蔓激化90级") {
-            expectDmg = (attributeToUse * percent + 1447 * 1.25 * emv) * critExpectv;
-            critDmg = (attributeToUse * percent + 1447 * 1.25 * emv) * (1 + CRITDMG / 100);
+            expectDmg = (attributeToUse * percent + 1447 * 1.25 * emv) * critExpectv * resAndLevel;
+            critDmg = (attributeToUse * percent + 1447 * 1.25 * emv) * (1 + CRITDMG / 100) * resAndLevel;
             direct = attributeToUse * percent / ((attributeToUse * percent + 1447 * 1.25 * emv));
             element = 1 - direct;
         }
@@ -134,6 +187,9 @@ function DashboardPage() {
         const updatedTableData = tableData.map((item) => {
             if (item.id === id) {
                 const currentValue = parseFloat(item.content);
+                if (id === 3 || id === 4) {
+                    adjustment = adjustment/100 * parseFloat(tableData[id + 2].content);
+                }
                 if (!isNaN(currentValue)) {
                     return { ...item, content: (currentValue + adjustment).toFixed(1) };
                 }
@@ -163,7 +219,17 @@ function DashboardPage() {
                     </Select>
                 </div>
                 <div style={tableContainerStyle}>
-                    <Table dataSource={tableData} pagination={false}>
+                    <Table
+                        dataSource={tableData.filter(item => {
+                            if (calculationMode === 'ATK' && (item.id === 4 || item.id === 6))
+                                return false
+                            if (calculationMode === 'MaxHP' && (item.id === 3 || item.id === 5))
+                                return false
+                            if (damageMultiplier === "无" && item.id === 2)
+                                return false
+                            return true
+                        })}
+                        pagination={false}>
                         <Column title="属性" dataIndex="name" key="name" />
                         <Column
                             title="数值"
